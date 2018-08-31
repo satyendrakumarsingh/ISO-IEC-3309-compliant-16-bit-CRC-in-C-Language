ad5bacd619a5193022b5900fab8c37c4376f4ce9
Satyendra Kumar Singh <hi@satyendrakumarsingh.com>
2018-08-31 19:11:43 +0530

    Create iso_iec_3309_crc.c

---
+++ iso_iec_3309_crc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+    unsigned short crcCalculator(const unsigned char* data_p, unsigned char length){
+        unsigned char x;
+        unsigned short crc = 0xFFFF;
+
+        while(length--) {
+            x = crc >> 8 ^ *data_p++;
+            x ^= x>>4;
+            crc = (crc << 8) ^ ((unsigned short)(x << 12)) ^ ((unsigned short)(x <<5)) ^ ((unsigned short)x);
+        }
+        
+        return crc;
+    }
+        
+	int main() {
+		unsigned char plainTextData[] = "20101021202164403849800004306041552202409800";
+		printf("%X", crcCalculator(plainTextData, sizeof(plainTextData)-1));
+	}
